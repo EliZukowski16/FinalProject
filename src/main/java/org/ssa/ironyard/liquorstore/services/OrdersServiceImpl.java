27d03e93ba6d17a82ee76b1d668b64c2674c47b4
Eli Zukowski <eli.zukowski@gmail.com>
2016-10-12 16:55:43 -0400

    Implemented several search methods in Order Service, need to test

--- src/main/java/org/ssa/ironyard/liquorstore/services/OrdersServiceImpl.java
+++ src/main/java/org/ssa/ironyard/liquorstore/services/OrdersServiceImpl.java
@@ -17,10 +17,10 @@ import org.ssa.ironyard.liquorstore.model.Product;
 @Service
 public class OrdersServiceImpl implements OrdersService
 {
-    
+
     DAOOrder daoOrder;
     DAOProduct daoProduct;
-    
+
     @Autowired
     public OrdersServiceImpl(DAOOrder daoOrder, DAOProduct daoProduct)
     {
@@ -46,34 +46,54 @@ public class OrdersServiceImpl implements OrdersService
     @Transactional
     public List<Order> readOrdersByCustomer(Integer customerID)
     {
-        // TODO Auto-generated method stub
-        return null;
+        List<Integer> ids = new ArrayList<>();
+
+        if (customerID == null)
+            return new ArrayList<>();
+
+        ids.add(customerID);
+
+        return daoOrder.readOrdersByCustomers(ids);
+
     }
 
     @Override
     @Transactional
     public List<Order> readOrdersByProduct(Integer productID)
     {
-        // TODO Auto-generated method stub
-        return null;
+        List<Integer> ids = new ArrayList<>();
+
+        if (productID == null)
+            return new ArrayList<>();
+
+        ids.add(productID);
+
+        return daoOrder.readOrdersByProduct(ids);
     }
 
     @Override
     @Transactional
     public List<Order> readOrdersByCoreProdcut(Integer coreProductID)
     {
-        // TODO Auto-generated method stub
-        return null;
+        List<Integer> ids = new ArrayList<>();
+
+        if (coreProductID == null)
+            return new ArrayList<>();
+
+        ids.add(coreProductID);
+
+        return daoOrder.readOrderByCoreProduct(ids);
     }
 
     @Override
     @Transactional
     public Order editOrder(Order order)
     {
-        if(daoOrder.read(order.getId()) == null)
+        if (daoOrder.read(order.getId()) == null)
             return null;
-        
-        Order ord = new Order(order.getId(),order.getCustomer(),order.getDate(),order.getTotal(),order.getoD(),order.getOrderStatus());
+
+        Order ord = new Order(order.getId(), order.getCustomer(), order.getDate(), order.getTotal(), order.getoD(),
+                order.getOrderStatus());
         return daoOrder.update(ord);
     }
 
@@ -82,79 +102,76 @@ public class OrdersServiceImpl implements OrdersService
     public Order addOrder(Order order)
     {
 
-
         List<OrderDetail> odList = order.getoD();
 
-        
         List<OrderDetail> outOfStock = new ArrayList();
         Order ordOutOfStock;
-        
+
         for (int i = 0; i < odList.size(); i++)
         {
-            Product p =  daoProduct.read(odList.get(i).getProduct().getId());
-            if(p.getInventory() <= 0)
+            Product p = daoProduct.read(odList.get(i).getProduct().getId());
+            if (p.getInventory() <= 0)
             {
-                OrderDetail od = new OrderDetail(p,null,null);
+                OrderDetail od = new OrderDetail(p, null, null);
                 outOfStock.add(od);
             }
-            
-           
+
         }
-        
-        if(outOfStock.size() > 0)
+
+        if (outOfStock.size() > 0)
         {
-            ordOutOfStock = new Order(null,null,null,null,outOfStock,null);
+            ordOutOfStock = new Order(null, null, null, null, outOfStock, null);
             return ordOutOfStock;
         }
-        
+
         List<Integer> listProductIds = new ArrayList();
-        
+
         for (int i = 0; i < order.getoD().size(); i++)
         {
             listProductIds.add(order.getoD().get(i).getProduct().getId());
         }
-        
+
         List<Product> products = daoProduct.readByIds(listProductIds);
-        
+
         List<OrderDetail> odPriceChange = new ArrayList();
-        
+
         for (int i = 0; i < products.size(); i++)
         {
             for (int j = 0; j < order.getoD().size(); j++)
             {
-                if(products.get(i).getId() == order.getoD().get(j).getProduct().getId())
+                if (products.get(i).getId() == order.getoD().get(j).getProduct().getId())
                 {
-                    if(products.get(i).getPrice().compareTo(order.getoD().get(j).getProduct().getPrice()) != 0)
+                    if (products.get(i).getPrice().compareTo(order.getoD().get(j).getProduct().getPrice()) != 0)
                     {
                         Product pPrice = products.get(i);
-                        OrderDetail odPrice = new OrderDetail(pPrice,null,null);
+                        OrderDetail odPrice = new OrderDetail(pPrice, null, null);
                         odPriceChange.add(odPrice);
                     }
                 }
             }
-            
+
         }
-        
-        if(odPriceChange.size() > 0)
+
+        if (odPriceChange.size() > 0)
         {
-           Order ordPriceChange = new Order(null,null,order.getDate(),null,odPriceChange,null);
-           return ordPriceChange;
+            Order ordPriceChange = new Order(null, null, order.getDate(), null, odPriceChange, null);
+            return ordPriceChange;
         }
-        
-        //get price
-        Order ord = new Order(order.getCustomer(),order.getDate(),order.getTotal(),order.getoD(),order.getOrderStatus());
 
-        return daoOrder.insert(ord);          
-   }
+        // get price
+        Order ord = new Order(order.getCustomer(), order.getDate(), order.getTotal(), order.getoD(),
+                order.getOrderStatus());
 
+        return daoOrder.insert(ord);
+    }
 
     @Override
     @Transactional
     public boolean deleteOrder(Integer id)
     {
-        if(daoOrder.read(id) == null)
+        if (daoOrder.read(id) == null)
             return false;
-        
+
         return daoOrder.delete(id);
     }
 
@@ -168,8 +185,7 @@ public class OrdersServiceImpl implements OrdersService
     @Override
     public List<Order> searchTimeFrame(LocalDate date1, LocalDate date2)
     {
-        // TODO Auto-generated method stub
-        return null;
+        return daoOrder.readOrdersInTimeFrame(date1, date2);
     }
 
     @Override
@@ -206,9 +222,5 @@ public class OrdersServiceImpl implements OrdersService
         // TODO Auto-generated method stub
         return null;
     }
-    
-    
-    
-    
 
 }
