cdb47530fe91b04ae8f158b1b5e1a81c682ebcde
Eli Zukowski <eli.zukowski@gmail.com>
2016-10-17 10:48:13 -0400

    Admin view pending/unfulfilled orders, approve and reject

--- src/main/java/org/ssa/ironyard/liquorstore/controller/AdminController.java
+++ src/main/java/org/ssa/ironyard/liquorstore/controller/AdminController.java
@@ -1,5 +1,6 @@
 package org.ssa.ironyard.liquorstore.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,8 +14,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.ssa.ironyard.liquorstore.model.Customer;
+import org.ssa.ironyard.liquorstore.model.Order;
 import org.ssa.ironyard.liquorstore.model.Product;
 import org.ssa.ironyard.liquorstore.services.AdminServiceImpl;
 import org.ssa.ironyard.liquorstore.services.AnalyticsServiceImpl;
@@ -28,7 +31,7 @@ import org.ssa.ironyard.liquorstore.services.SalesServiceImpl;
 @RequestMapping("/TheBeerGuys/admin")
 public class AdminController
 {
-    
+
     @Autowired
     AdminServiceImpl adminService;
     @Autowired
@@ -43,10 +46,11 @@ public class AdminController
     ProductServiceImpl productService;
     @Autowired
     SalesServiceImpl salesService;
-    
+
     static Logger LOGGER = LogManager.getLogger(AdminController.class);
-    
-    public AdminController(AdminServiceImpl as, AnalyticsServiceImpl ans, CoreProductServiceImpl cps, CustomerServiceImpl cs, OrdersServiceImpl os, ProductServiceImpl ps, SalesServiceImpl ss)
+
+    public AdminController(AdminServiceImpl as, AnalyticsServiceImpl ans, CoreProductServiceImpl cps,
+            CustomerServiceImpl cs, OrdersServiceImpl os, ProductServiceImpl ps, SalesServiceImpl ss)
     {
         adminService = as;
         analyticsService = ans;
@@ -54,114 +58,163 @@ public class AdminController
         customerService = cs;
         orderService = os;
         productService = ps;
-        salesService = ss;       
-        
+        salesService = ss;
+
     }
-    
-    
-    
-    @RequestMapping(value="/customers", method = RequestMethod.GET)
-    public ResponseEntity<Map<String,List<Customer>>> getAllCustomers()
+
+    @RequestMapping(value = "/customers", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<Map<String, List<Customer>>> getAllCustomers()
     {
         LOGGER.info("Returning list of customers");
-        Map<String,List<Customer>> response = new HashMap<>();
+        Map<String, List<Customer>> response = new HashMap<>();
         List<Customer> customers = customerService.readAllCustomers();
-        
-        if(customers == null)
+
+        if (customers == null)
             response.put("error", customers);
         else
             response.put("success", customers);
-        
-        LOGGER.trace("This was a success",response);
-        
+
+        LOGGER.trace("This was a success", response);
+
         return ResponseEntity.ok().header("The Beer Guy Admin", "Customers").body(response);
-        
+
     }
-    
-    @RequestMapping(value="/customers/{id}", method = RequestMethod.GET)
-    public ResponseEntity<Map<String,Customer>> getCustomer(@PathVariable String id)
+
+    @RequestMapping(value = "/customers/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<Map<String, Customer>> getCustomer(@PathVariable String id)
     {
         LOGGER.info("returning single customer");
-        Map<String,Customer> response = new HashMap<>();
+        Map<String, Customer> response = new HashMap<>();
         Customer customer = customerService.readCustomer(Integer.parseInt(id));
-        
-        LOGGER.info("Found customer",customer.getId(),customer.getUserName(),customer.getPassword(),customer.getFirstName(),customer.getLastName(),
-                customer.getAddress(),customer.getBirthDate());
-        
-        if(customer == null)
+
+        if (customer == null)
             response.put("error", customer);
         else
+        {
+            LOGGER.info("Found customer", customer.getId(), customer.getUserName(), customer.getPassword(),
+                    customer.getFirstName(), customer.getLastName(), customer.getAddress(), customer.getBirthDate());
             response.put("success", customer);
-        
+        }
+
         return ResponseEntity.ok().header("Customers", "Customer").body(response);
-        
+
     }
-    
-    @RequestMapping(value="/customer/{id}",method = RequestMethod.DELETE)
+
+    @RequestMapping(value = "/customer/{id}", method = RequestMethod.DELETE)
     public boolean deleteCustomer(@PathVariable String id)
     {
         LOGGER.info("deleting single customer");
-        
+
         Integer cusID = Integer.parseInt(id);
-        
+
         boolean deleteSuccess = customerService.deleteCustomer(cusID);
-        LOGGER.info("Delete was successful ",deleteSuccess);
-        
+        LOGGER.info("Delete was successful ", deleteSuccess);
+
         return deleteSuccess;
-        
-        
-        
+
     }
-    
-    @RequestMapping(value="/products", method = RequestMethod.GET)
-    public ResponseEntity<Map<String,List<Product>>> getProducts()
+
+    @RequestMapping(value = "/products", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<Map<String, List<Product>>> getProducts()
     {
-        Map<String,List<Product>> response = new HashMap<>();
+        Map<String, List<Product>> response = new HashMap<>();
         List<Product> productsList = productService.readAllProducts();
-        
+
         LOGGER.info("getttinga all products");
-        
-        if(productsList == null)
+
+        if (productsList == null)
             response.put("error", productsList);
         else
             response.put("success", productsList);
-        
-        LOGGER.trace("checking for a success",response);
-        
+
+        LOGGER.trace("checking for a success", response);
+
         return ResponseEntity.ok().header("The Beer Guys Admin", "Products").body(response);
-        
-        
+
     }
-    
-    @RequestMapping(value="/products/{id}", method = RequestMethod.GET)
-    public ResponseEntity<Map<String,Product>> getProdcut(@PathVariable String id)
+
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<Map<String, Product>> getProdcut(@PathVariable String id)
     {
-        Map<String,Product> response = new HashMap<>();
+        Map<String, Product> response = new HashMap<>();
         LOGGER.info("getting one product");
-        
+
         Integer prodID = Integer.parseInt(id);
         Product prod = productService.readProduct(prodID);
-        
-        if(prod == null)
+
+        if (prod == null)
             response.put("error", prod);
         else
             response.put("success", prod);
-            
-        LOGGER.trace("checking for a success",response);
-        
+
+        LOGGER.trace("checking for a success", response);
+
         return ResponseEntity.ok().header("Products", "Product Detail").body(response);
     }
-    
-    @RequestMapping(value="/products",method = RequestMethod.POST)
-    public ResponseEntity<Map<String,Product>> addProduct(HttpServletRequest request)
+
+    @RequestMapping(value = "/products", method = RequestMethod.POST)
+    public ResponseEntity<Map<String, Product>> addProduct(HttpServletRequest request)
     {
         return null;
     }
-    
-    @RequestMapping(value="/products/{id}",method = RequestMethod.PUT)
-    public ResponseEntity<Map<String,Product>> editProduct(@PathVariable String id, HttpServletRequest request)
+
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Map<String, Product>> editProduct(@PathVariable String id, HttpServletRequest request)
     {
         return null;
     }
-    
+
+    @RequestMapping(value = "/orders", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<Map<String, List<Order>>> getAllOrders()
+    {
+        Map<String, List<Order>> response = new HashMap<>();
+
+        List<Order> orders = orderService.readAllOrder();
+
+        if (orders.isEmpty())
+            response.put("error", new ArrayList<>());
+        else
+            response.put("success", orders);
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    @RequestMapping(value = "/orders/pending", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<Map<String, List<Order>>> getUnfulfilledOrders()
+    {
+        Map<String, List<Order>> response = new HashMap<>();
+
+        List<Order> orders = orderService.readUnfulfilledOrders();
+
+        if (orders.isEmpty())
+            response.put("error", new ArrayList<>());
+        else
+            response.put("success", orders);
+
+        return ResponseEntity.ok().body(response);
+    }
+
+    @RequestMapping(value = "/orders/pending/{id}/approve", method = RequestMethod.POST)
+    public Boolean approveOrder(@PathVariable String id)
+    {
+        Integer orderID = Integer.parseInt(id);
+
+        return orderService.approveOrder(orderID);
+
+    }
+
+    @RequestMapping(value = "/orders/pending/{id}/reject", method = RequestMethod.POST)
+    public Boolean rejectOrder(@PathVariable String id)
+    {
+        Integer orderID = Integer.parseInt(id);
+
+        return orderService.rejectOrder(orderID);
+    }
+
 }
