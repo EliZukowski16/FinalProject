1d8463dd14f361759fb4cbeecac36c15f10584b1
Laura Slemp <lslemp13@yahoo.com>
2016-10-18 09:15:29 -0400

    admin setup

--- src/main/resources/static/js/controllers/AdminHomeController.js
+++ src/main/resources/static/js/controllers/AdminHomeController.js
@@ -1,3 +1,32 @@
-/**
- * 
- */
+angular
+	.module("liquorStoreAdmin")
+	.controller("AdminHomeController", homeCtrl)	
+	homeCtrl.$inject=['$http']
+
+	function homeCtrl($http)
+	{
+		var ctrl = this;
+		ctrl.pending = [];
+		
+		
+		ctrl.getPending = function()
+		{	
+		
+		$http({
+			url: location.pathname +"/orders/pending",
+			method: 'GET',
+		}).success(function(response) {		
+			
+			ctrl.pending= response.data.success;
+	  		console.log(ctrl.pending);
+	  		
+		}).error(function(response){
+			console.log("http get error");
+		})
+		};
+		
+		ctrl.getPending();
+		
+		
+		
+	}
