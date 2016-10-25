35f3c252bf2a480f1d877ea0c3dbb93af4c88ddc
Eli Zukowski <eli.zukowski@gmail.com>
2016-10-25 10:30:43 -0400

    date formatting for customer orders'

--- src/main/resources/static/js/controllers/CustomerOrdersController.js
+++ src/main/resources/static/js/controllers/CustomerOrdersController.js
@@ -1,33 +1,42 @@
 angular
 	.module("liquorStore")
-	.controller("CustomerOrdersController", orderCtrl)	
-	orderCtrl.$inject=['$http']
+	.controller("CustomerOrdersController", orderCtrl)
+orderCtrl.$inject = [ '$http' ]
 
-	function orderCtrl($http)
-	{
-		var ctrl = this;
-		ctrl.isCollapsed = true;
-		ctrl.orders = [];	
-	    ctrl.sortType = '';
-	    ctrl.sortReverse = false;
-		ctrl.ordersExpanded = true;
-		
-		ctrl.getOrders = function()
-		{
-	    	
-			$http({
-				url: location.pathname +"/Orders",
-				method: 'GET',
-			}).then(function(response) {		
-				
-				ctrl.orders= response.data.success;
-		  		console.log(ctrl.orders);
-			})
-			
-		};
-		
-		ctrl.getOrders();
-	
-		
+function orderCtrl($http) {
+	var ctrl = this;
+	ctrl.isCollapsed = true;
+	ctrl.orders = [];
+	ctrl.sortType = '';
+	ctrl.sortReverse = false;
+	ctrl.ordersExpanded = true;
+
+	ctrl.getOrders = function() {
+
+		$http({
+			url : location.pathname + "/Orders",
+			method : 'GET',
+		}).then(function(response) {
+
+			ctrl.orders = response.data.success;
+			console.log(ctrl.orders);
+			ctrl.convertDates();
+
+		})
+
+	};
+
+	ctrl.getOrders();
+
+
+	ctrl.convertDates = function() {
+		for (let i = 0; i < ctrl.orders.length; i++) {
+
+
+			ctrl.orders[i].deliveryDate = new Date(ctrl.orders[i].date.year, ctrl.orders[i].date.monthValue - 1, ctrl.orders[i].date.dayOfMonth);
+			ctrl.orders[i].orderDate = new Date(ctrl.orders[i].timeOfOrder.year, ctrl.orders[i].timeOfOrder.monthValue - 1, ctrl.orders[i].timeOfOrder.dayOfMonth, ctrl.orders[i].timeOfOrder.hour, ctrl.orders[i].timeOfOrder.minute, ctrl.orders[i].timeOfOrder.second)
+		}
 	}
-	
+
+
+}
