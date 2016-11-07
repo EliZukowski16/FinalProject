9ad4e2750dc61274138ad4d23568e2055bd9c016
Eli Zukowski <eli.zukowski@gmail.com>
2016-11-06 21:13:55 -0500

    Set datasource to cleardb url

--- src/main/java/org/ssa/ironyard/liquorstore/config/DataSourceConfiguration.java
+++ src/main/java/org/ssa/ironyard/liquorstore/config/DataSourceConfiguration.java
@@ -1,6 +1,10 @@
 package org.ssa.ironyard.liquorstore.config;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,16 +12,26 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Configuration
-public class DataSourceConfiguration 
+public class DataSourceConfiguration
 {
+    
+
     static final String DATABASE_URL = "jdbc:mysql://localhost/liquor_store?" + "user=root&password=root&" + "useServerPrepStmts=true";
     static final Logger LOGGER = LogManager.getLogger(DataSourceConfiguration.class);
     @Bean
-    public DataSource datasource()
+    public DataSource datasource() throws URISyntaxException
     {
+        URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+        
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+        
         LOGGER.debug("YEAH ANnotation based processing is working");
         MysqlDataSource mysqlds = new MysqlDataSource();
-        mysqlds.setURL(DATABASE_URL);
+        mysqlds.setURL(dbUrl);
+        mysqlds.setUser(username);
+        mysqlds.setPassword(password);
         return mysqlds;
     }
 
